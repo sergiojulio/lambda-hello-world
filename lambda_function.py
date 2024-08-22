ba9c9c8cb89c4e8e7bfc2a4ca52e785a52c80202
sergiojulio <sergiojulio@gmail.com>
2024-08-22 12:00:41 -0400

    Test 1

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
 import sys
 def handler(event, context):
-    return 'Hello from AWS Lambda using Python' + sys.version + '!' 
+    return 'Hello from AWS Lambda using Python' + sys.version + ' from GitHub!' 
 
 
 
